51b0ec97611899877665bd2d44f5bee69a46c3c3
MeesCode <meesgamez@gmail.com>
2025-05-29 17:25:10 +0200

    a bit of cleanup

--- main/main.c
+++ main/main.c
@@ -19,17 +19,9 @@ uint8_t image[128 * 128 * 3]; // RGB888
 
 void on_draw(pngle_t *pngle, uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint8_t rgba[4])
 {
-    uint8_t r = rgba[0]; // 0 - 255
-    uint8_t g = rgba[1]; // 0 - 255
-    uint8_t b = rgba[2]; // 0 - 255
-    // uint8_t a = rgba[3]; // 0: fully transparent, 255: fully opaque
-
-    // if (a) printf("put pixel at (%d, %d) with color #%02x%02x%02x\n", x, y, r, g, b);
-    // ESP_LOGI(TAG, "put pixel at (%d, %d) with color #%02x%02x%02x", (int)x, (int)y, (int)r, (int)g, (int)b);
-
-    image[(y * 128 + x) * 3 + 0] = r; // Red
-    image[(y * 128 + x) * 3 + 1] = g; // Green
-    image[(y * 128 + x) * 3 + 2] = b; // Blue
+    image[(y * 128 + x) * 3 + 0] = rgba[0]; // Red
+    image[(y * 128 + x) * 3 + 1] = rgba[1]; // Green
+    image[(y * 128 + x) * 3 + 2] = rgba[2]; // Blue
 }
 
 void ST7789(void *pvParameters)
@@ -56,32 +48,28 @@ void ST7789(void *pvParameters)
   lcdDrawSquare(&dev, 0, 0, CONFIG_WIDTH, CONFIG_HEIGHT, BLACK);
   ESP_LOGI(TAG, "Display Initialized");
 
-
-  pngle_t *pngle = pngle_new();
-
-  pngle_set_draw_callback(pngle, on_draw);
-
+  ESP_LOGI(TAG, "Loading PNG file from SD card...");
   FILE *fp = fopen("/sdcard/test.png", "rb");
 
+  pngle_t *pngle = pngle_new();
+  pngle_set_draw_callback(pngle, on_draw);
+  
   char buf[1024];
   int remain = 0;
   int len;
 	while (!feof(fp)) {
 		if (remain >= sizeof(buf)) {
 			ESP_LOGE(__FUNCTION__, "Buffer exceeded");
-			while(1) vTaskDelay(1);
 		}
 
-		int len = fread(buf + remain, 1, sizeof(buf) - remain, fp);
+		len = fread(buf + remain, 1, sizeof(buf) - remain, fp);
 		if (len <= 0) {
-			//printf("EOF\n");
 			break;
 		}
 
 		int fed = pngle_feed(pngle, buf, remain + len);
 		if (fed < 0) {
 			ESP_LOGE(__FUNCTION__, "ERROR; %s", pngle_error(pngle));
-			while(1) vTaskDelay(1);
 		}
 
 		remain = remain + len - fed;
@@ -90,22 +78,20 @@ void ST7789(void *pvParameters)
 
   ESP_LOGI(TAG, "PNG file read complete");
   fclose(fp);
+  pngle_destroy(pngle);
 
   int dy = 1;
   int py = 20;
 
+  // logic loop
 	while(1) {
 		lcdDrawImage(&dev, image, 20, py, 128, 128);
 
     py += dy;
     if (py >= CONFIG_HEIGHT - 20 - 128 || py <= 20) {
       dy = -dy; // reverse direction
-      ESP_LOGI(TAG, "Change direction, py=%d", py);
     }
 	}
-
-  free(image);
-	pngle_destroy(pngle);
 
 	// never reach here
 	while (1) {
